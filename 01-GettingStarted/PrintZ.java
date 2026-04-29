c1a1e3083e1671784a1c048d5f09cf3def1eab5d
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-29 07:34:18 +0530

    Create PrintZ.java

---
+++ 01-GettingStarted/PrintZ.java
@@ -0,0 +1,10 @@
+//Ques 1:- You are required to print a "Z" of size 5 using "*"
+public class PrintZ{
+  public static void main(String[] args){
+    System.out.println("*****");
+    System.out.println("   * ");
+    System.out.println("  *  ");
+    System.out.println(" *   ");
+    System.out.println("*****");
+  }
+}
