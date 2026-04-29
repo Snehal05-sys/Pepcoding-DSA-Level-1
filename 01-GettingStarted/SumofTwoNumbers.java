98cc709acf00a521636b660bec16a3cc62afcc63
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-29 07:38:46 +0530

    Create SumofTwoNumbers.java

---
+++ 01-GettingStarted/SumofTwoNumbers.java
@@ -0,0 +1,11 @@
+//Ques 2:- Print the Sum of Two Numbers
+public class SumofTwoNumbers{
+  public static void main(String[] args){
+    int x=15;
+    int y=10;
+    int sum=x+y;
+    System.out.println("X="+x);
+    System.out.println("Y="+y);
+    System.out.println("Sum of X and Y is="+sum);
+  }
+}
