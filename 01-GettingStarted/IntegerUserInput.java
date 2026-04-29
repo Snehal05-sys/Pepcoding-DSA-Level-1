f128020d9c57f849bb8535321e3c7ea13821a98b
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-29 08:13:43 +0530

    Create IntegerUserInput.java

---
+++ 01-GettingStarted/IntegerUserInput.java
@@ -0,0 +1,10 @@
+//Print User input for Integer.
+public class IntegerUserInput{
+  public static void main(String[] args){
+    Scanner scn= new Scanner(System.in);
+    int num= scn.nextInt();
+    for (int i=0; i<=n; i++){
+      System.out.printl(i);
+    }
+  }
+}
