3f36312f6ff449f7b65f68e995fa7b4f8f9a1de6
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-29 08:08:58 +0530

    Create PrintNumbers.java

---
+++ 01-GettingStarted/PrintNumbers.java
@@ -0,0 +1,11 @@
+//Ques 6:- Print 1-9 Numbers using While Loop.
+public class PrintNumbers{
+  public static void main(String[] args){
+    int x=1;
+    while(x<=9){
+      System.out.println(x);
+      i++;
+    }
+    System.out.println("done");
+  }
+}
