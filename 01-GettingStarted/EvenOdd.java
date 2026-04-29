2b8914dc776210a5be60bc318157cb8ca2e4594b
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-29 07:42:33 +0530

    Create EvenOdd.java

---
+++ 01-GettingStarted/EvenOdd.java
@@ -0,0 +1,12 @@
+//Ques 3:- Check if the number is even or odd.
+public class EvenOdd{
+  public static void main(String[] args){
+    int num=10;
+    if(num%2==0){
+      System.out.println(num+"is Even Number");
+    }
+    else{
+      System.out.println(num+"is Odd Number");
+    }
+  }
+}
