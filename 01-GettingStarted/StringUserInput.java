dcaecbd6a6577280bafdb877ffb4b80bd1a1e88f
Snehal05-sys <devkarsnehal0@gmail.com>
2026-05-02 19:52:07 +0530

    Create StringUserInput.java

---
+++ 01-GettingStarted/StringUserInput.java
@@ -0,0 +1,8 @@
+public class main{
+  public static void main(String[] args){
+    Scanner scn= new Scanner(System.in);
+    String name=scn.nextLine();
+    System.out.println("Hello"+name);
+  }
+}
+    
