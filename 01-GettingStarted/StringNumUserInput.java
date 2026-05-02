f8544e2984548074309eed04041ab51745465c2c
Snehal05-sys <devkarsnehal0@gmail.com>
2026-05-02 19:57:37 +0530

    Create StringNumUserInput.java

---
+++ 01-GettingStarted/StringNumUserInput.java
@@ -0,0 +1,11 @@
+public class main{
+  public static class main(String[] args){
+  Scanner scn = new Scanner(System.in);
+  int n= integer.parseInt(scn.nextLine());
+  String name= scn.nextLine();
+  System.out.println("Dear"+name+"Here is the counting");
+  for(int i=1; i<=n; i++){
+    System.out.println(i);
+  }
+}
+}
